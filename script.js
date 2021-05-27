e0cba58effc8a940bfca0efd6d6c579d0a9230a6
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-05-27 18:04:49 -0300

    js

---
+++ script.js
@@ -0,0 +1,20 @@
+function insert(num){
+  let numero = document.getElementById('resultado').innerHTML
+  document.getElementById('resultado').innerHTML = numero + num
+}
+
+function clean() {
+  document.getElementById('resultado').innerHTML = ""
+}
+
+function back() {
+  let menosUm = document.getElementById('resultado').innerHTML
+  document.getElementById('resultado').innerHTML = menosUm.substring(0, menosUm.length -1) 
+}
+
+function calcular() {
+  let resultado = document.getElementById('resultado').innerHTML
+  if(resultado) {
+    document.getElementById('resultado').innerHTML = eval(resultado)
+  }
+} 
